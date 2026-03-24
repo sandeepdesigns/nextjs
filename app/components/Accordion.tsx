c7c99c6f0308a0c721b1b6061f2e722193a6a2aa
Sandeep Tripathi <sandeep@iws.in>
2026-03-24 12:22:45 +0530

    change

--- app/components/Accordion.tsx
+++ app/components/Accordion.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { useState } from "react";
 
-export default function Accordion({ items = [] }) {
-  const [activeIndex, setActiveIndex] = useState(0); // first open
+// 👇 type define karo
+type AccordionItem = {
+  title: string;
+  content: string;
+};
 
-  const toggleAccordion = (index) => {
+export default function Accordion({ items = [] }: { items: AccordionItem[] }) {
+  const [activeIndex, setActiveIndex] = useState<number | null>(0);
+
+  const toggleAccordion = (index: number) => {
     setActiveIndex(activeIndex === index ? null : index);
   };
 
@@ -12,8 +18,9 @@ export default function Accordion({ items = [] }) {
     <figure className="flex flex-col gap-1 rounded-xl bg-gray-950/5 p-1 inset-ring inset-ring-gray-950/5 dark:bg-white/10 dark:inset-ring-white/10">
       <div className="not-prose rounded-lg bg-white outline outline-white/5 dark:bg-gray-950/50 p-5">
         <div className="space-y-4">
-          {items.map((item, index) => (
+          {items.map((item: AccordionItem, index: number) => (
             <div key={index} className="pb-2">
+              
               {/* Button */}
               <button
                 onClick={() => toggleAccordion(index)}
@@ -22,16 +29,16 @@ export default function Accordion({ items = [] }) {
                 <span>{item.title}</span>
 
                 <svg
-                className={`w-4 h-4 ml-auto transition-transform duration-300 ${
-                  activeIndex === index ? "rotate-180" : ""
-                }`}
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
-              </svg>
+                  className={`w-4 h-4 ml-auto transition-transform duration-300 ${
+                    activeIndex === index ? "rotate-180" : ""
+                  }`}
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+                </svg>
               </button>
 
               {/* Content */}
